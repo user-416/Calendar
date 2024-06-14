c275c6555d3dbbfb7a39a88029d06d9af5cc7c4d
user_416 <tgao124@gmail.com>
2024-06-14 15:58:32 -0400

    deleted unneeded shema

--- server/src/model/email.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const emailSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true }
-});
-
-const Email = mongoose.model('Email', emailSchema);
-
-module.exports = Email;
