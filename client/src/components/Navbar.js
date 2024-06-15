de176928a05d781cbd2da784bad1fdcde1190960
kevin1285 <132208181+kevin1285@users.noreply.github.com>
2024-06-14 21:11:55 -0400

    Create Navbar.js, change the "Calendar App" heading to be part of the navbar.

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import './Main.css';
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-title">CalConnect</div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
