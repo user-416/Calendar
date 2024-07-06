bcf2a567d54024d11eba0149629ebe5917fcaaff
user_416 <tgao124@gmail.com>
2024-07-06 17:33:40 -0400

    nvm

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: true,
+    secure: false,
     httpOnly: true, 
     maxAge: 1000 * 60 * 60 * 24 // 24hr
   }
