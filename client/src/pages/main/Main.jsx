f7fafe72697a71cff67622bf18e660ec52062359
kevinma <2006kevinma@gmail.com>
2024-08-19 14:01:14 -0400

    add time inputs dummy proof

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext, useRef} from 'react';
 import calendarService from '../../services/calendar';
 import { useNavigate } from 'react-router-dom';
 import CSS from './Main.module.css';
@@ -10,18 +10,22 @@ import { AuthContext } from '../../contexts/AuthContext';
 import authService from '../../services/auth';
 const Main = () => {
     const [eventName, setEventName] = useState('');
-    const [startTime, setStartTime] = useState('9:00');
-    const [endTime, setEndTime] = useState('5:00');
+    const [startTime, setStartTime] = useState('09:00');
+    const [endTime, setEndTime] = useState('05:00');
     const [isAMStart, setIsAMStart] = useState(true);
     const [isAMEnd, setIsAMEnd] = useState(false);
     const defaultTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     const [timezone, setTimezone] = useState(defaultTimezone);
 
     const [selectedDates, setSelectedDates] = useState(new Set());
+    const {authStatus, setAuthStatus} = useContext(AuthContext);
 
+    const startFieldRef = useRef(), endFieldRef = useRef();
     const navigate = useNavigate(); 
 
-    const {authStatus, setAuthStatus} = useContext(AuthContext);
+    const timePattern = "^(0?[1-9]|1[0-2]):[0-5][0-9]$";
+
+    
     useEffect(() => {
         authService.getAuth()
             .then(data => {
@@ -38,6 +42,14 @@ const Main = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if(!validateTimeInput(startTime, startFieldRef.current)){
+            startFieldRef.current.reportValidity();
+            return;
+        }
+        if(!validateTimeInput(endTime, endFieldRef.current)){
+            endFieldRef.current.reportValidity();
+            return;
+        }
         const startHHMM = TimeUtil.toMilitaryTime(startTime, isAMStart)
         const endHHMM = TimeUtil.toMilitaryTime(endTime, isAMEnd);
         const startUTC = TimeUtil.convertToUTC(startHHMM, timezone);
@@ -63,7 +75,30 @@ const Main = () => {
         }
     };
 
+    const handleTimeInput = (e, setTime) => {
+        e.target.setCustomValidity('');
+        setTime(e.target.value);
+    }
 
+    const validateTimeInput = (input, field) => {
+        if(!/^[0-9]?[0-9]:[0-9][0-9]$/.test(input)){
+            field.setCustomValidity('Invalid format. Please use HH:MM.');
+            return false;
+        }else{
+            const [hours, minutes] = input.split(':');
+            if(!/^(0?[1-9]|1[0-2])$/.test(hours)){
+                field.setCustomValidity('Hours must be between 1 and 12');
+                return false;
+            }
+            else if(!/^[0-5][0-9]$/.test(minutes)){
+                field.setCustomValidity('Minutes must be between 0 and 59.');
+                return false;
+            }
+            return true;
+        }
+    }
+
+    
     return (
         <div className={CSS.mainContainer}>
             <div className={CSS.contentContainer}>
@@ -85,10 +120,12 @@ const Main = () => {
 
                     <div className={CSS.timeInputs}>
                         <input
+                            ref={startFieldRef}
                             type="text"
                             value={startTime}
                             className={CSS.timeField}
-                            onChange={(e) => setStartTime(e.target.value)}
+                            onChange={(e) => handleTimeInput(e, setStartTime)}
+                            required
                         />
                         <button type="button" onClick={() => setIsAMStart(true)} className={`${CSS.toggleButton} ${isAMStart ? CSS.active : ''}`}>AM</button>
                         <button type="button" onClick={() => setIsAMStart(false)} className={`${CSS.toggleButton} ${!isAMStart ? CSS.active : ''}`}>PM</button>
@@ -96,10 +133,12 @@ const Main = () => {
                         <div className={CSS.dash}></div>
 
                         <input
+                            ref={endFieldRef}
                             type="text"
                             value={endTime}
                             className={CSS.timeField}
-                            onChange={(e) => setEndTime(e.target.value)}
+                            onChange={(e) => handleTimeInput(e, setEndTime)}
+                            required
                         />
                         <button type="button" onClick={() => setIsAMEnd(true)} className={`${CSS.toggleButton} ${isAMEnd ? CSS.active : ''}`}>AM</button>
                         <button type="button" onClick={() => setIsAMEnd(false)} className={`${CSS.toggleButton} ${!isAMEnd ? CSS.active : ''}`}>PM</button>
